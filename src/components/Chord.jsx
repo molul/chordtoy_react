555177c80f9b61856e262684d3edd23fc6d7b33b
molul <luismorcillo@gmail.com>
2023-10-25 01:20:44 +0200

    Starting chord calculating from base note with chordType

--- src/components/Chord.jsx
+++ src/components/Chord.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line react/prop-types
-const Chord = ({ chordNote, chord, playFunc }) => {
+const Chord = ({ chordNote, chordType, playFunc }) => {
   return (
     <div
-      className="bg-sky-700 hover:bg-sky-600 transition-colors text-center rounded cursor-pointer text-sm lg:text-base w-16 lg:w-20 px-2 lg:px-4 py-4"
-      onClick={() => playFunc(chordNote, chord)}
+      className="bg-sky-700 hover:bg-sky-600 transition-colors text-center rounded cursor-pointer text-xs lg:text-base w-14 lg:w-20 px-2 lg:px-4 py-4"
+      onClick={() => playFunc(chordNote, chordType)}
     >
-      {chordNote + chord}
+      {chordNote + chordType}
     </div>
   );
 };
--- src/components/ChordList.jsx
+++ src/components/ChordList.jsx
@@ -6,6 +6,7 @@ import Chord from "./Chord";
 
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 const now = Tone.now();
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 const ChordList = () => {
   const [currentTones, setCurrentTones] = useState([]);
@@ -15,9 +16,10 @@ const ChordList = () => {
   };
 
   //play a middle 'C' for the duration of an 8th note
-  const play = (chordNote, chord) => {
+  const play = (chordNote, chordType) => {
     stopCurrentNotes();
-    const chordToPlay = chords[chordNote][chord];
+    calculateChord(chordNote, chordType);
+    const chordToPlay = chords[chordNote][chordType];
 
     setCurrentTones(chordToPlay);
 
@@ -28,18 +30,43 @@ const ChordList = () => {
     }, 1);
   };
 
-  const chordTypes = ["maj", "maj7", "m"];
+  const getRelativeNote = (note, semitones) => {
+    let relNoteIndex = notes.indexOf(note) + semitones;
+    if (relNoteIndex > notes.length) {
+      relNoteIndex -= notes.length;
+    }
+
+    // console.log("REL NOTE INDEX: " + relNoteIndex);
+    // console.log("REL NOTE: " + notes[relNoteIndex]);
+    const relNote = notes[relNoteIndex];
+    return { index: relNoteIndex, note: relNote };
+  };
+
+  const calculateChord = (note, chordType) => {
+    const noteIndex = notes.indexOf(note);
+    const baseOctave = noteIndex <= 2 ? 4 : 5;
+    console.log("BASE OCTAVE: " + baseOctave);
+    console.log("NOTE INDEX: " + noteIndex);
+    if (chordType === "maj") {
+      let relNote = getRelativeNote(note, 4);
+      console.log(relNote.index);
+      console.log(relNote.note);
+    }
+    return note;
+  };
+
+  const chordTypes = ["maj", "maj7", "m", "m7"];
 
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-      <div className="flex flex-row gap-4 items-center justify-center">
-        <div className="p-4 text-center rounded cursor-pointer text-sm lg:text-base w-16 lg:w-20 px-2 lg:px-4 py-4">
-          Note
+    <div className="flex flex-col gap-2 items-center justify-center">
+      <div className="flex flex-row gap-2 items-center justify-center">
+        <div className="text-center rounded cursor-pointer text-sm lg:text-base w-14 lg:w-20 px-2 lg:px-4 word-wrap">
+          Note / Chord
         </div>
         {chordTypes.map((chordType, index) => (
           <div
             key={index}
-            className="p-4 text-center rounded cursor-pointer text-sm lg:text-base w-16 lg:w-20 px-2 lg:px-4 py-4"
+            className="bg-zinc-700 text-center rounded cursor-pointer text-sm lg:text-base w-14 lg:w-20 px-2 lg:px-4 py-4"
           >
             {chordType}
           </div>
@@ -48,14 +75,18 @@ const ChordList = () => {
 
       {Object.keys(chords).map((chordNote, index) => {
         return (
-          <div key={index} className="flex flex-row gap-4 items-center">
-            <div className="border border-white p-4 text-center rounded cursor-pointer text-sm lg:text-base w-16 lg:w-20 px-2 lg:px-4 py-4">
+          <div key={index} className="flex flex-row gap-2 items-center">
+            <div className=" bg-zinc-700 text-center rounded cursor-pointer text-xs lg:text-base w-14 lg:w-20 px-2 lg:px-4 py-4 font-bold">
               {chordNote}
             </div>
-            {Object.keys(chords[chordNote]).map((chord, index2) => {
+            {Object.keys(chords[chordNote]).map((chordType, index2) => {
               return (
                 <div key={`${index}-${index2}`}>
-                  <Chord chordNote={chordNote} chord={chord} playFunc={play} />
+                  <Chord
+                    chordNote={chordNote}
+                    chordType={chordType}
+                    playFunc={play}
+                  />
                 </div>
               );
             })}
