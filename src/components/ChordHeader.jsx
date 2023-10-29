9da29151b5968986085e15b4c121a48cd91b57d1
molul <luismorcillo@gmail.com>
2023-10-29 17:13:41 +0100

    Actualizar ChordHeader.jsx

--- src/components/ChordHeader.jsx
+++ src/components/ChordHeader.jsx
@@ -2,6 +2,7 @@
 
 const ChordHeader = ({ note, playNote }) => {
   return (
+
     <div
       className=" bg-sky-700 hover:bg-sky-600 transition-colors text-center rounded cursor-pointer text-xs lg:text-base px-1 lg:px-4 py-3 font-bold  shadow-lg"
       onClick={() => playNote(note)}
