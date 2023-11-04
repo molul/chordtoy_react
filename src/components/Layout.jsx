40c01b3462a236c23c595ca3df63ccf842a86a2d
molul <luismorcillo@gmail.com>
2023-11-04 10:46:54 +0100

    Less padding

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import ChordsGrid from "./ChordsGrid";
 const Layout = () => {
   return (
     // <div className="text-white flex flex-col justify-between min-h-screen bg-black bg-opacity-20 overflow-hidden">
-    <div className="min-h-screen flex flex-col bg-zinc-600 p-4 w-full text-white">
+    <div className="min-h-screen flex flex-col bg-zinc-600 p-2 w-full text-white">
       <ChordsGrid />
     </div>
   );
